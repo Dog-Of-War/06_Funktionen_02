6abd94892063a9c0427354e63b7345385b9eba0d
Peter <schmidt.multimedia@gmail.com>
2021-11-23 11:47:07 +0100

    05 / update ausgabe()

--- script_01.js
+++ script_01.js
@@ -4,7 +4,7 @@
 0. a+b / a-b/ a*b / a/b  // ergebnis c
 1. Dateneingabe + -überprüfung 
 2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
+3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
 
@@ -52,9 +52,14 @@ function addieren(a,b) {
 }
 
 // Modul: Konsolenausgabe | Test:
-// ausgabe("Hallo, Welt!")
+// ausgabe("Hallo, Welt!");
+// ausgabe(20);
 function ausgabe(outputData) {
-    console.log(outputData);
+    if (typeof outputData == "number") {
+        console.log("Das Ergebnis ist: " + outputData);
+    } else {
+        console.log(outputData);
+    }
 }
 
 /*
