498f57ff5f1e8684f351ad9350ef27c0eebf2bcf
Peter <schmidt.multimedia@gmail.com>
2021-11-23 10:47:44 +0100

    03 / subtraktion / multiplikation / Wienerwald

--- script_01.js
+++ script_01.js
@@ -9,6 +9,24 @@
 */
 
 
+
+
+// Modul: Multiplikation a * b |  Test:
+// ausgabe(multiplizieren(33))
+// ausgabe(multiplizieren(-3,2))
+// ausgabe(multiplizieren(0,2))
+function multiplizieren(a,b) {
+    return a * b ;
+}
+
+// Modul: Subtraktion a - b |  Test:
+// ausgabe(subtrahieren(2.0,1.1));
+// ausgabe(subtrahieren(2,10));
+// ausgabe(subtrahieren(2,0));
+function subtrahieren(a,b) {
+    return a - b;
+}
+
 // Modul: Addition a + b |  Test:
 // ausgabe(addieren(2,1));
 // ausgabe(addieren(2,-5));
@@ -22,3 +40,28 @@ function addieren(a,b) {
 function ausgabe(outputData) {
     console.log(outputData);
 }
+
+/*
+ Heute bleibt die Küche kalt ...
+ wir gehen in den Wienerwald. 
+*/
+
+/*
+ servieren(anrichten(gemuesekoch(),saucenkoch()));
+
+function servieren(teller) {
+    console.log("Einmal einen " + teller +  " Bitte sehr!")
+}
+
+function anrichten(kartoffeln,soße) {
+    return "Teller mit: "+ kartoffeln + " " + soße;
+}
+
+function gemuesekoch(){
+    return "gekochte Kartoffeln";
+ }
+ 
+ function saucenkoch() {
+     return "lecker Sauce";
+ }
+ */
